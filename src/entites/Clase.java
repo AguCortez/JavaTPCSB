fe5e7c9d5f306356e7c8cc292a21da992eba42f8
SilSavio <90285956+SilSavio@users.noreply.github.com>
2022-02-11 23:45:50 -0300

    util profesional con errores
    f

--- src/entites/Clase.java
+++ src/entites/Clase.java
@@ -62,5 +62,17 @@ public class Clase {
 		this.legajo_prof = legajo_prof;
 	}
 	
-
+}
+public Clase(int total_cupo, int id, String nombre, int actual_cupos, String dia, String hora, String descripcion, int legajo_prof) {
+	this.setTotal_cupo(total_cupo);
+	this.setId(id);
+	this.setNombre(nombre);
+	this.setActual_cupos(actual_cupos);
+	this.setDia(dia);
+	this.setHora(hora);
+	this.setDescripcion(descripcion);
+	this.setLegajo_prof(legajo_prof);
+	
+	
+}
 }
