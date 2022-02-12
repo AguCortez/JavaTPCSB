e64b5955112c30d447e3e63651cfdf33053ef33a
SilSavio <90285956+SilSavio@users.noreply.github.com>
2022-02-11 23:13:38 -0300

    util clase

--- src/util/claseUtil.java
+++ src/util/claseUtil.java
@@ -1,5 +1,35 @@
 package util;
+import entites.Clase; 
+
+import java.util.LinkedList;
+
+import data.dataClase;
 
 public class claseUtil {
+	
+	
+	public void altaClase(int total_cupo, int id, String nombre, int actual_cupos, String dia, time hora, String descripcion, int legajo_prof) {
+		Clase nClase=new Clase(total_cupo, id, nombre, actual_cupos, dia, hora, descripcion, legajo_prof);
+		dataClase ds=new dataClase();
+		ds.add(nClase);
 
-}
+	}
+	public void bajaClase(int id) {
+		dataClase ds= new dataClase();
+		ds.delete(id);
+	}
+
+	public void modClase(Clase c, int idviejo) {
+		dataClase ds= new dataClase();
+		ds.update(c, idviejo);
+	}
+
+	public LinkedList<Clase> getAll(){
+		dataClase ds=new dataClase();
+		return ds.getAll();
+	}
+
+	public Clase getByid(int id) {
+		dataClase ds=new dataClase();
+		return ds.getOne(id);
+	}
