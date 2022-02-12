c880989a13fefe8511ae50d81c82771b0204a808
SilSavio <90285956+SilSavio@users.noreply.github.com>
2022-02-11 23:38:14 -0300

    data tiporutina corregida

--- src/data/dataTiporutina.java
+++ src/data/dataTiporutina.java
@@ -1,6 +1,13 @@
 package data;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+
+import entites.*;
+
 
 
 public class dataTiporutina {
@@ -14,14 +21,14 @@ public class dataTiporutina {
 	
 	
 	
-public LinkedList<Tiporutina> getAll(){
+public LinkedList<TipoRutina> getAll(){
 		
 		Statement stmt=null;
 		ResultSet rs=null;
 		
 		
 		
-		LinkedList<Tiporutina> tip= new LinkedList<>();
+		LinkedList<TipoRutina> tip= new LinkedList<>();
 		
 		try {
 			stmt= dbConector.getInstancia().getConn().createStatement();
@@ -29,9 +36,9 @@ public LinkedList<Tiporutina> getAll(){
 			
 			if(rs!=null) {
 				while(rs.next()) {
-					Tiporutina t=new Tiporutina();
+					TipoRutina t=new TipoRutina();
 					
-					t.setIdtipo(rs.getInt("id tipo"));
+					t.setId_tipo(rs.getInt("id tipo"));
 					t.setDescripcion(rs.getString("descripcion"));
 					
 					tip.add(t);
@@ -54,8 +61,8 @@ public LinkedList<Tiporutina> getAll(){
 		
 		return tip;
 	}
-	public Tiporutina getOne(int idtipo) {
-		Tiporutina laTiporutina= null;
+	public TipoRutina getOne(int idtipo) {
+		TipoRutina laTipoRutina= null;
 		PreparedStatement ps=null;
 		ResultSet rs=null;
 		try {
@@ -65,9 +72,9 @@ public LinkedList<Tiporutina> getAll(){
 			
 				if(rs!=null && rs.next())
 				{
-					laTiporutina = new Tiporutina();
-					laTiporutina.setIdtipo(rs.getInt("id tipo"));
-					laTiporutina.setDescripcion(rs.getString("descripcion"));
+					laTipoRutina = new TipoRutina();
+					laTipoRutina.setId_tipo(rs.getInt("id tipo"));
+					laTipoRutina.setDescripcion(rs.getString("descripcion"));
 				}
 				
 			} 
@@ -86,15 +93,15 @@ public LinkedList<Tiporutina> getAll(){
 				e.printStackTrace();
 			}
 		}		
-		return laTiporutina;
+		return laTipoRutina;
 	
 	}
-	public void add(Tiporutina t) {
+	public void add(TipoRutina t) {
 		PreparedStatement ps=null;
 		
 		try {
 			ps=dbConector.getInstancia().getConn().prepareStatement(newTiporutina);
-			ps.setInt(1, t.getIdtipo());
+			ps.setInt(1, t.getId_tipo());
 			ps.setString(2, t.getDescripcion());
 			
 			ps.executeUpdate();
@@ -132,16 +139,13 @@ public LinkedList<Tiporutina> getAll(){
             catch (SQLException e) {
             	e.printStackTrace();
             }
-		}
 		
-		
-	}
-	public void update (Tiporutina t, int idtipoold) {
+	public void update (TipoRutina t, int idtipoold) {
 		PreparedStatement ps=null;
 		try {
 			ps=dbConector.getInstancia().getConn().prepareStatement(modifica);
 			
-			ps.setInt(1, t.getIdtipo());
+			ps.setInt(1, t.getId_tipo());
 			ps.setString(2, t.getDescripcion());
 			ps.setInt(3, idtipoold);
 			ps.executeUpdate();
@@ -153,54 +157,16 @@ public LinkedList<Tiporutina> getAll(){
 		finally {
             try {               
                 if(ps!=null)ps.close();
-                dbConector.getInstancia().releaseConn();
+                dbConector.getInstancia().releaseConn(); 
             } 
             catch (SQLException e) {
             	e.printStackTrace();
             }
-		}
+		 }
 		
 		
-		}
-	
-	public  LinkedList<Tiporutina>  buscabyIdtipo(int idtipo) {
-		LinkedList<Tiporutina> Tiporutinas= new LinkedList<>();
-		PreparedStatement ps=null;
-		ResultSet rs=null;
-		try {
-			ps=dbConector.getInstancia().getConn().prepareStatement(buscabyIdtipo);
-			ps.setString(1, idtipo);
-			rs=ps.executeQuery();
-			
-				if(rs!=null && rs.next())
-				{while(rs!=null && rs.next()) {
-					Tiporutina t=new Tiporutina();
-					
-					
-					t.setIdtipo(rs.getInt("id tipo"));
-					t.setDescripcion(rs.getString("descripcion"));
-									
-					
-					Tiporutinas.add(t);
-				}
-				}
-			} 
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
-		finally 
-		{
-			try {
-				if(rs!=null) {rs.close();}
-				if(ps!=null) {ps.close();}
-				dbConector.getInstancia().releaseConn();
-			} 
-			catch (SQLException e) {
-				e.printStackTrace();
-			}		}		
-		return Tiporutinas;
-	}
+		
+		
 
 		
 	
