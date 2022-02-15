3db1a8ffde2685f87e3d63e609a6530706b9bf64
SilSavio <90285956+SilSavio@users.noreply.github.com>
2022-02-14 21:24:18 -0300

    util reserva

--- src/util/reservaUtil.java
+++ src/util/reservaUtil.java
@@ -1,6 +1,35 @@
 package util;
+import entites.Reserva; 
 
-public class reservaUtil {
+import java.util.LinkedList;
 
+import data.dataReserva;
+
+public class reservasUtil {
 	
-}
+	
+	public void altaReserva(int id_reserva, date fecha, int dni, String codigo) {
+		Reserva nReserva=new Reserva(id_reserva, fecha, dni, codigo );
+		dataReserva ds=new dataReserva();
+		ds.add(nReserva);
+
+	}
+	public void bajaReserva(int id_reserva) {
+		dataReserva ds= new dataReserva();
+		ds.delete(id_reserva);
+	}
+
+	public void modReserva(Reserva r, int id_reservaviejo) {
+		dataReserva ds= new dataReserva();
+		ds.update(r, id_reservaviejo);
+	}
+
+	public LinkedList<Reserva> getAll(){
+		dataReserva ds=new dataReserva();
+		return ds.getAll();
+	}
+
+	public Reserva getByIdreserva(int id_reserva) {
+		dataReserva ds=new dataReserva();
+		return ds.getOne(id_reserva);
+	}
